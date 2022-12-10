20dc4b34b727e0ef2bc038abc22f9cfa6e433c52
joshua-fajardo <FAJARDO.JOSHUA@UE.EDU.PH>
2022-12-11 02:01:51 +0800

    Update login.php

--- NEW/login.php
+++ NEW/login.php
@@ -129,17 +129,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       }
       .form-control:focus {
           color: #000;
-          
+          background-color: #fff;
           border:2px solid #B4d7ff;
           outline: 0;
           box-shadow: none;
       }
       
       .btn {
-        background: #B4d7ff;
-        border: #B4d7ff;
-      }
-      .btn:hover {
         background: #B4d7ff;
         border: #B4d7ff;
       }

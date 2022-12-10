33252a08a0745e92e9f7ebd9df5a2d07096c19c6
joshua-fajardo <FAJARDO.JOSHUA@UE.EDU.PH>
2022-12-10 22:59:56 +0800

    Updated register.php
    Modified styles and format of Registration form (HTML)

--- NEW/register.php
+++ NEW/register.php
@@ -102,43 +102,78 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <title>Sign Up</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Muli'>
     <style>
-        body {
-            font: 14px sans-serif;
-        }
-
-        .wrapper {
-            width: 360px;
-            padding: 20px;
-        }
+      body{
+        background: #E8D426 !important;
+        font-family: 'Muli', sans-serif;
+        
+      }
+      h1{
+        color: #fff;
+        padding-bottom: 2rem;
+        font-weight: bold;
+      }
+      a{
+        color: #333;
+      }
+      a:hover{
+        color: #E8D426;
+        text-decoration: none;
+      }
+      .form-control:focus {
+          color: #000;
+          background-color: #fff;
+          border:2px solid #E8D426;
+          outline: 0;
+          box-shadow: none;
+      }
+      
+      .btn{
+        background: #E8D426;
+        border: #E8D426;
+      }
+      .btn:hover {
+        background: #E8D426;
+        border: #E8D426;
+      }
     </style>
 </head>
 
 <body>
-    <div class="wrapper">
-        <h2>Sign Up</h2>
-        <p>Please fill this form to create an account.</p>
+<div class="pt-5">
+  <h1 class="text-center">Utility Due Date and Subscription Tracker</h1>
+    <div class="container">
+    <div class="row">
+        <div class="col-md-5 mx-auto">
+            <div class="card card-body">
+
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group">
+            <div class="form-group required">
                 <label>Username</label>
                 <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
                 <span class="invalid-feedback"><?php echo $username_err; ?></span>
             </div>
-            <div class="form-group">
+            <div class="form-group required">
                 <label>Password</label>
                 <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
                 <span class="invalid-feedback"><?php echo $password_err; ?></span>
             </div>
-            <div class="form-group">
+            <div class="form-group required">
                 <label>Confirm Password</label>
                 <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
                 <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
             </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Submit">
-                <input type="reset" class="btn btn-secondary ml-2" value="Reset">
+
+            <div class="form-group pt-1">
+                    <button type="submit" class="btn btn-primary btn-block">Log In</button>
+                    <button type="reset" class="btn btn-seondary ml-2">Reset</button>
             </div>
-            <p>Already have an account? <a href="login.php">Login here</a>.</p>
+            
+            <p class="small-xl pt-3 text-center">
+                <span class= "text-muted">Already have an account? </span>
+                <a href="login.php">Login here</a>
+            </p>
         </form>
     </div>
 </body>

5059cdcc78e575425045670f29295692c5412d0b
asocastro <77965653+asocastro@users.noreply.github.com>
2022-12-12 16:28:43 +0800

    Update config.php

--- NEW/config.php
+++ NEW/config.php
@@ -1,15 +1,32 @@
 <?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
+
+
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'project_sdf');
 
-/* Attempt to connect to MySQL database */
+
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-// Check connection
 if ($mysqli === false) {
     die("ERROR: Could not connect. " . $mysqli->connect_error);
 }
+
+
+
+/*
+define('DB_SERVER', '185.27.134.10');
+define('DB_USERNAME', 'epiz_33184146');
+define('DB_PASSWORD', 'BX3Hy6zlCZG');
+define('DB_NAME', 'epiz_33184146_project_sdf');
+
+
+$mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if ($mysqli === false) {
+    die("ERROR: Could not connect. " . $mysqli->connect_error);
+}
+*/
+
+

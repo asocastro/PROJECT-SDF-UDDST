3daf5dcd3eb7fbaeb1ac1bfba38964c63dfe4244
joshua-fajardo <FAJARDO.JOSHUA@UE.EDU.PH>
2022-12-18 20:42:25 +0800

    Updated create.php
    Captions modified

--- NEW/create.php
+++ NEW/create.php
@@ -191,7 +191,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <div class="row">
                 <div class="col-md-12">
                     <h2 class="mt-5">Create Record</h2>
-                    <p>Please fill this form and submit to add user record to the database.</p>
+                    <p>Please fill this form and submit to add record to the database.</p>
                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                         <div class="form-group">
                             <label>Category</label>

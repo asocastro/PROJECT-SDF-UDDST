a2de6d9150fd6e1746a2261ac004ed9b907e386a
joshua-fajardo <FAJARDO.JOSHUA@UE.EDU.PH>
2022-12-18 20:31:02 +0800

    Updated update.php
    Captions modified

--- NEW/update.php
+++ NEW/update.php
@@ -266,7 +266,7 @@ if (isset($_POST["id"]) && !empty($_POST["id"])) {
             <div class="row">
                 <div class="col-md-12">
                     <h2 class="mt-5">Update Record</h2>
-                    <p>Please edit the input values and submit to update the employee record.</p>
+                    <p>Please edit the input values and submit to update the record.</p>
                     <form action="<?php echo htmlspecialchars(basename($_SERVER['REQUEST_URI'])); ?>" method="post">
                         <div class="form-group">
                             <label>Category</label>

ba8dce23978b94c593432341e1666d7badadbba2
joshua-fajardo <FAJARDO.JOSHUA@UE.EDU.PH>
2022-12-18 19:05:27 +0800

    Updated config.php
    Comments were moved

--- NEW/config.php
+++ NEW/config.php
@@ -1,6 +1,6 @@
 <?php
 
-/*
+
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
@@ -12,10 +12,10 @@ $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if ($mysqli === false) {
     die("ERROR: Could not connect. " . $mysqli->connect_error);
 }
-*/
 
 
 
+/*
 define('DB_SERVER', 'sql.freedb.tech');
 define('DB_USERNAME', 'freedb_testt');
 define('DB_PASSWORD', 'GX4x$VCbVhx9#Bt');
@@ -28,5 +28,5 @@ if ($mysqli === false) {
     die("ERROR: Could not connect. " . $mysqli->connect_error);
 }
 
-
+*/
 
